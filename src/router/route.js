795386af2a86cb70fce5fa28a6b0ab86dec61ac8
tushar199627 <tusharbarai6@gmail.com>
2022-09-07 12:13:18 +0530

    updated

--- src/router/route.js
+++ src/router/route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require("../controller/userController")
+const userController = require("../Controller/userController")
 
 
 
